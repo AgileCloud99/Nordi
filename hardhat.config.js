ba931df3178c96618f81e6e1456306dcff7fbc22
AgileCloud99 <nobody@example.com>
2025-03-14 17:02:50 +0100

    added base network to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,4 +3,9 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+networks: {
+    base: {
+      url: "https://mainnet.base.org",
+      chainId: 8453,
+      accounts: [`0x${process.env.PRIVATE_KEY}`] }
 };
